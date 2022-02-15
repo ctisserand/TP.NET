0a3e7e91b194c3d532ce0a1baf8071d70e904455
lotfi.m'hamed@etu.univ-cotedazur.fr <lotfi.m'hamed@etu.univ-cotedazur.fr>
2022-02-15 11:29:42 +0100

    init database

--- ASP.Server/Database/DBInitializer.cs
+++ ASP.Server/Database/DBInitializer.cs
@@ -19,20 +19,64 @@ namespace ASP.Server.Database
 
             Genre SF, Classic, Romance, Thriller;
             bookDbContext.Genre.AddRange(
-                SF = new Genre(),
-                Classic = new Genre(),
-                Romance = new Genre(),
+                SF = new Genre()
+                {
+                    Name = "SF",
+                    Description = "Romance bla bla bla"
+                },
+                Classic = new Genre()
+                {
+                    Name="classic",
+                    Description = "Romance bla bla bla"
+                },
+                Romance = new Genre()
+                {
+                    Name = "Romance",
+                    Description = "Romance bla bla bla"
+                },
                 Thriller = new Genre()
+                {
+                    Name = "Thriller",
+                    Description = " Thriller bla bla bla"
+                }
             );
             bookDbContext.SaveChanges();
 
             // Une fois les moèles complété Vous pouvez faire directement
             // new Book() { Author = "xxx", Name = "yyy", Price = n.nnf, Content = "ccc", Genres = new() { Romance, Thriller } }
             bookDbContext.Books.AddRange(
-                new Book(), 
-                new Book(),
-                new Book(),
                 new Book()
+                {
+                    Name ="kalila wa demna",
+                    Author = "Antonin",
+                    Price = "49",
+                    Content ="livre de show ",
+                    Genders = new List<Genre> {SF}
+                }, 
+                new Book()
+                {
+                    Name = "Harry Potter",
+                    Author = "Khadijat",
+                    Price = "49",
+                    Content = "livre de show ",
+                    Genders = new List<Genre> { Classic }
+                },
+                new Book()
+                {
+                    Name = "Voyage au bout de la nuit",
+                    Author = "Lotfi",
+                    Price = "49",
+                    Content = "livre de show ",
+                    Genders = new List<Genre> { Romance }
+                },
+                new Book()
+                {
+                    Name = "Le seigneur des anneaux",
+                    Author = "Rachid",
+                    Price = "49",
+                    Content = "livre de show ",
+                    Genders = new List<Genre> { Thriller }
+                }
             );
             // Vous pouvez initialiser la BDD ici
 
